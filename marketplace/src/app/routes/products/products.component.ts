6537d69118dec5736bd37759e146aec0a498ea59
Gimenez <jgimenez@Almato.com>
2025-02-18 09:30:30 +0100

    Carga de productos desde Strapi

--- marketplace/src/app/routes/products/products.component.ts
+++ marketplace/src/app/routes/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  private httpClient = inject(HttpClient);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  products = signal<any[]>([]);
 
+  ngOnInit(){
+    this.httpClient.get('http://localhost:1337/api/products').subscribe({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      next: (response: any) => {
+        console.log(response);
+        this.products.set(response.data);
+      },
+      error: (error) => {
+        console.log("Error al recopilar productos", error);
+      }
+    });
+  }
+  
+  
 }
