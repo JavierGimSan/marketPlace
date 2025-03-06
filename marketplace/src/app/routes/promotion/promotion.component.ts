fecae9918249df6e577622c3f59d33ef66f77901
Javier Gimenez <javi.gimsa@gmail.com>
2025-03-06 12:23:59 +0100

    Corrección en Promotion y Promotions. Ahora aparece el loader de carga y si hay un error aparece la imagen de error.

--- marketplace/src/app/routes/promotion/promotion.component.ts
+++ marketplace/src/app/routes/promotion/promotion.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 import { PromotionsService } from '../promotions/promotions.component';
 import { Router } from '@angular/router';
+import { ErrorImage } from '../../shared/services/error-image.service';
 
 @Component({
   selector: 'app-promotion',
@@ -20,6 +21,7 @@ export class PromotionComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   promotionDiscount = signal<any>(0); //GUARDO EL DESCUENTO DE LA PROMOCIÓN PARA OPERAR CON EL PRECIO
   private promotionsService = inject(PromotionsService);
+    private errorImage = inject(ErrorImage);
 
   constructor(private router: Router) {}
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -27,24 +29,30 @@ export class PromotionComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   promotionProducts = signal<any[]>([]);
 
+  isFetching = signal(false);
+
+  loadError = false;
+  errorImageUrl = '';
+
   ngOnInit() {
     const url = window.location.href;
     const promotionId = url.split('promotion/')[1];
-    // this.errorImageUrl = this.errorImage.getErrorImage();
-    // this.isFetching.set(true);
+    this.errorImageUrl = this.errorImage.getErrorImage();
+    this.isFetching.set(true);
     this.promotionsService.loadPromotionProducts(promotionId).subscribe({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       next: (response: any) => {
         this.promotionProducts.set(response.data[0].products);
       },
-      // error: error => {
-      //   console.log('Error al recopilar productos', error);
-      //   this.loadError = true;
-      //   this.isFetching.set(false);
-      // },
-      // complete: ()=> {
-      //   this.isFetching.set(false);
-      // }
+      error: error => {
+        console.log('Error al recopilar productos', error);
+        this.loadError = true;
+        this.isFetching.set(false);
+      },
+      complete: ()=> {
+        this.isFetching.set(false);
+      },
+      
     });
 
     this.promotionsService.loadPromotion(promotionId).subscribe({
@@ -55,24 +63,8 @@ export class PromotionComponent implements OnInit {
         console.log(typeof(this.promotionDiscount))
       }
     })
-
-    // this.promotionsService.loadProductPromotions(productId).subscribe({
-    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //   next: (response: any) => {
-    //     this.promotionProducts.set(response.data[0].products);
-    //     console.log(response.data[0].products);
-    //   },
-    //   error: error => {
-    //     console.log('Error al recopilar productos', error);
-    //     //   this.loadError = true;
-    //     //   this.isFetching.set(false);
-    //   },
-    //   // complete: ()=> {
-    //   //   this.isFetching.set(false);
-    //   // }
-    // });
   }
-
+  
   //MOSTRAR INFORMACIÓN DE UN PRODUCTO
   showInfo(productId: string) {
     this.router.navigate([`/product/${productId}`]);
--- marketplace/src/app/routes/promotions/promotions.component.ts
+++ marketplace/src/app/routes/promotions/promotions.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { PromotionsService } from '../../shared/services/promotions.service';
 import { Router } from '@angular/router';
+import { ErrorImage } from '../../shared/services/error-image.service';
 
 @Component({
   selector: 'app-promotions',
@@ -9,21 +10,37 @@ import { Router } from '@angular/router';
   styleUrl: './promotions.component.scss'
 })
 export class PromotionsComponent implements OnInit{
-
   constructor(private router: Router){}
 
   promotionsService = inject(PromotionsService);
+  
+  private errorImage = inject(ErrorImage);
+
+  isFetching = signal(false);
+
+  loadError = false;
+  errorImageUrl = '';  
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   promotions = signal<any[]>([]);
   ngOnInit() {
+    this.errorImageUrl = this.errorImage.getErrorImage();
+    this.isFetching.set(true);
     this.promotionsService.loadPromotions().subscribe({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       next: (response: any) => {
         this.promotions.set(response.data);
         console.log(response.data);
       },
-    })    
+      error: error => {
+        console.log('Error al recopilar productos', error);
+        this.loadError = true;
+        this.isFetching.set(false);        
+      },
+      complete: ()=> {
+        this.isFetching.set(false);
+      }
+    });    
   }
 
   showPromotionProducts(promotionId: string) {
