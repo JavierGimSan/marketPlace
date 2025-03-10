d2bba47f187111d865e1625823691730e32128de
Javier Gimenez <javi.gimsa@gmail.com>
2025-03-10 09:43:14 +0100

    Avatar implementado

--- marketplace/src/app/theme/header/header.component.ts
+++ marketplace/src/app/theme/header/header.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ProfileMenuComponent } from "../profile-menu/profile-menu.component";
+import { ProfileMenuComponent } from '../profile-menu/profile-menu.component';
 
 @Component({
   selector: 'app-header',
   imports: [RouterModule, ProfileMenuComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
   dropdownEsVisible = false;
 
-cambiarEstadoMenu() {
-  this.dropdownEsVisible = !this.dropdownEsVisible;
-  console.log(this.dropdownEsVisible);
-}
+  cambiarEstadoMenu() {
+    this.dropdownEsVisible = !this.dropdownEsVisible;
+    console.log(this.dropdownEsVisible);
+  }
+
 
 }
