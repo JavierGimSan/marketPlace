22f6d8528912daa7ffb8ab7d23d4d54a1127a9e0
Javier Gimenez <javi.gimsa@gmail.com>
2025-03-12 14:19:14 +0100

    Añadido método para introducir productos en el carrito

--- marketplace/src/app/routes/product/product.component.ts
+++ marketplace/src/app/routes/product/product.component.ts
@@ -3,6 +3,7 @@ import { ProductsService } from '../../shared/services/products.service';
 import { ErrorImage } from '../../shared/services/error-image.service';
 import { Router } from '@angular/router';
 import { ShoppingCartService } from '../../shared/services/shopping-cart.service';
+import { CartItem } from '../../shared/interfaces/cartItem.interface';
 
 @Component({
   selector: 'app-product',
@@ -11,6 +12,7 @@ import { ShoppingCartService } from '../../shared/services/shopping-cart.service
   styleUrl: './product.component.scss',
 })
 export class ProductComponent implements OnInit{
+
   private productsService = inject(ProductsService);
   private errorImage = inject(ErrorImage);
   private router = inject(Router);
@@ -26,13 +28,13 @@ export class ProductComponent implements OnInit{
   loadError = false;
   errorImageUrl = this.errorImage.getErrorImage();
 
-
+  url = window.location.href;
+  productId = this.url.split('product/')[1];
+  
   ngOnInit() {
     this.isFetching.set(true);
-    const url = window.location.href;
-    const productId = url.split('product/')[1];
 
-    this.productsService.loadProduct(productId).subscribe({
+    this.productsService.loadProduct(this.productId).subscribe({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       next: (response: any) => {
         this.product = response.data;
@@ -47,7 +49,7 @@ export class ProductComponent implements OnInit{
       }
     });
 
-    this.productsService.loadProductPromotions(productId).subscribe({
+    this.productsService.loadProductPromotions(this.productId).subscribe({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       next: (response: any) => {
         console.log(response.data[0].promotions);
@@ -72,10 +74,29 @@ export class ProductComponent implements OnInit{
     return this.shoppingCartService.itemsCount;
   }
 
-  addToCart(){ //Suma la cantidad del producto al total del carrito. Si cantidad = 9, carrito =+ 9.
-    this.shoppingCartService.setCartState();
-    this.shoppingCartService.setCountToZero();
-
-    console.log(this.shoppingCartService.cartState());
+  addToCart(productId:string){ //Suma la cantidad del producto al total del carrito. Si cantidad = 9, carrito =+ 9.
+    this.productsService.loadProduct(productId).subscribe({
+      next: (product) => {
+        const cartItem: CartItem = {
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          author: product.author,
+          image_url: product.image_url,
+          quantity: 1  // Inicializamos la cantidad en 1
+        };
+        this.shoppingCartService.addCartItem(cartItem);
+      },
+      error: (error) => {
+        console.error('Error fetching product:', error);
+      }
+    });
   }
+  
+  //   this.shoppingCartService.setCartState();
+  //   this.shoppingCartService.setCountToZero();
+
+  //   console.log(this.shoppingCartService.cartState());
+  // }
 }
+
