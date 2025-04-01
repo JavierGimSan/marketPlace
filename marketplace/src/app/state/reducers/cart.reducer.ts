1edbd0a8a7f08484f07042823b9214047c619762
Javier Gimenez <javi.gimsa@gmail.com>
2025-04-01 12:26:49 +0200

    Fix al añadir productos a cartItems. Ahora se pueden meter varios.

--- marketplace/src/app/state/reducers/cart.reducer.ts
+++ marketplace/src/app/state/reducers/cart.reducer.ts
@@ -19,7 +19,8 @@ export const cartReducer = createReducer(
   }),
 
   on(addToCartSuccess, (state, { item, quantity }) => { //Añadir producto al carrito
-    const existingIndex = state.cartItems.findIndex(cartItem => cartItem.name === item.name);
+    const existingIndex = state.cartItems.findIndex(cartItem => cartItem.documentId === item.documentId);
+    console.log("CART ITEMS???",state.cartItems)
     if (existingIndex !== -1) {
       
       const updatedCartItems = state.cartItems.map((cartItem, index) =>
